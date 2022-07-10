8af100f6d27f349dec07939844a0328f90be3e14
Artem Boiko <boiko.artem@protonmail.com>
2022-07-10 17:38:19 +0300

    2022-07-10: refactored list to map conversion

--- src/main/java/collection/ConversionListToMap_KB.java
+++ src/main/java/collection/ConversionListToMap_KB.java
@@ -30,12 +30,10 @@ public class ConversionListToMap_KB {
         userList.add(user3);
 
         Map<String, User> usersByEmail = convertInShortWay(userList);
-        usersByEmail.forEach((email, user) ->
-                                     logger.debug("e-mail: {}, user: {}",
-                                                  email,
-                                                  user));
+        usersByEmail.forEach((email, user) -> logger.debug("e-mail: {}, user: {}", email, user));
 
-        boolean isPresent = containsOnce(userList, new User(777, "name-777", "email-777"));
+        boolean isPresent = containsOnce(userList,
+                                         new User(777, "name-777", "email-777"));
         logger.info("isPresent = {}", isPresent); // false
 
         isPresent = containsOnce(userList, user1);
