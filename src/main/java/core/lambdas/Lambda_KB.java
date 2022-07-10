e74ae2c3f0817a7ea3670abdedeef31aa418a1f8
Artem Boiko <boiko.artem@protonmail.com>
2022-07-10 17:40:33 +0300

    2022-07-10: refactored lambdas

--- src/main/java/core/lambdas/Lambda_KB.java
+++ src/main/java/core/lambdas/Lambda_KB.java
@@ -1,9 +1,43 @@
 package core.lambdas;
 
+import collection.ConversionListToMap_KB;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Method;
+import java.util.function.Consumer;
+
 /**
- * // TODO add some description
+ * Lambdas, functional interfaces.
  *
  * @author Artem Boiko
  */
 public class Lambda_KB {
+    private static final Logger logger = LogManager.getLogger(ConversionListToMap_KB.class);
+
+    public static void main(String[] args) {
+        Consumer<String> stringConsumer = getAnonymousConsumer();
+        stringConsumer.accept("The anonymous message");
+
+        stringConsumer = getLambdaConsumer();
+        stringConsumer.accept("The lambda message");
+
+        Method[] methods = Lambda_KB.class.getDeclaredMethods();
+        for (Method method : methods) {
+            logger.info(method);
+        }
+    }
+
+    private static Consumer<String> getAnonymousConsumer() {
+        return new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                logger.info("Message NOT from lambda: {}", s);
+            }
+        };
+    }
+
+    private static Consumer<String> getLambdaConsumer() {
+        return s -> logger.info("Message NOT from lambda: {}", s);
+    }
 }
