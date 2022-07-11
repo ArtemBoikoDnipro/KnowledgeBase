0b2d17da60d3a8892508e8a7eb15056729a42992
Artem Boiko <boiko.artem@protonmail.com>
2022-07-11 17:15:09 +0300

    2022-07-11: Added maven surefire plugin config

---
+++ src/main/java/maven/Maven_KB.java
@@ -0,0 +1,33 @@
+package maven;
+
+/**
+ * Maven.
+ *
+ * @author Artem Boiko
+ */
+public class Maven_KB {
+    public static void main(String[] args) {
+        /*
+         * To run test via maven:
+         * add plugin
+         *
+         *
+         */
+//      <build>
+//        <plugins>
+//            <!--to include tests into maven goals-->
+//            <plugin>
+//                <groupId>org.apache.maven.plugins</groupId>
+//                <artifactId>maven-surefire-plugin</artifactId>
+//                <version>2.22.2</version>
+//                <configuration>
+//                    <includes>
+//                        <include>**/*Test.java</include>
+//                        <include>**/*IT.java</include>
+//                    </includes>
+//                </configuration>
+//            </plugin>
+//        </plugins>
+//    </build>
+    }
+}
